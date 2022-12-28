da29ee28bfd23d45dc37d7d2d5bde8c914a13e9f
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2022-12-28 20:31:34 +0100

    Karte vervollstaendigt

--- lib/karte.rb
+++ lib/karte.rb
@@ -1,24 +1,50 @@
 # frozen_string_literal: true
 
+require 'farbe'
+
+MIN_VALUE = 1
+MAX_VALUE = 9
+MAX_TRUMPF_VALUE = 4
+
 class Karte
   def initialize(wert:, farbe:)
     @wert = wert
     @farbe = farbe
   end
 
+  def self.max_trumpf
+    @max_trumpf ||= new(MAX_TRUMPF_VALUE, Farbe::RAKETE)
+  end
+
+  def self.nil_karte
+    @nil_karte ||= new(0, Farbe::ANTI_RAKETE)
+  end
+
+  def self.alle_normalen
+    @alle_normalen ||= NORMALE_FARBEN.flat_map { |f| (MIN_VALUE..MAX_VALUE).map { |w| new(f, w) } }
+  end
+
+  def self.alle_truempfe
+    @alle_truempfe ||= (MIN_VALUE..MAX_TRUMPF_VALUE).map { |w| new(f, w) }
+  end
+
+  def self.alle
+    @alle ||= @alle_normalen + @alle_truempfe
+  end
+
   attr_reader :wert, :farbe
 
   def schlaegt?(karte)
-    if (karte.farbe == :rakete) && ((@farbe != :rakete) || (@wert <= karte.wert))
-      false
-    elsif farbe == :rakete
-      true
-    elsif farbe == :antiRakete
-      false
-    elsif karte.farbe == :antiRakete
-      true
-    else
-      farbe == karte.farbe
-    end
+    @farbe.schlaegt?(karte.farbe) || @farbe == karte.farbe && @wert > karte.wert
+  end
+
+  def eql?(other)
+    self.class == other.class && @wert == other.wert && @farbe == other.farbe
+  end
+
+  alias == eql?
+
+  def hash
+    @hash ||= [self.class, @wert, @farbe].hash
   end
 end
