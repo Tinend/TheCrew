ce7b7cd244a9118eafb8756df65854398bca0ebc
Tinend <ulrich.brodowsky@gmx.net>
2022-12-28 21:06:16 +0100

    Rubocop

--- lib/stich.rb
+++ lib/stich.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-# verwaltet einen Stich und Karten die drauf gelegt werden
 require_relative 'karte'
 
+# verwaltet einen Stich und Karten die drauf gelegt werden
 class Stich
   def initialize
     @sieger = nil
-    @staerksteKarte = Karte.nil_karte
+    @staerkste_karte = Karte.nil_karte
     @karten = []
   end
 
   attr_reader :sieger, :karten
 
   def farbe
-    @staerksteKarte.farbe
+    @staerkste_karte.farbe
   end
   
   def legen(karte:, spieler:)
-    if karte.schlaegt?(@staerksteKarte)
-      @sieger = :spieler
-      @staerksteKarte = karte
+    if karte.schlaegt?(@staerkste_karte)
+      @sieger = spieler
+      @staerkste_karte = karte
     end
     @karten.push(karte)
   end
