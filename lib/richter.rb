e1649eba26a408d11befed70d5c1445e3af683c2
Tinend <ulrich.brodowsky@gmx.net>
2022-12-28 20:16:38 +0100

    Auftrag_Verwalter, Richter und Stich geadded

---
+++ lib/richter.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Richter
+  def initialize
+    @verloren = false
+    @gewonnen = false
+    @auftraege = []
+  end
+
+  attr_reader :gewonnen, :verloren
+
+  def auftraege_erhalten(auftraege)
+    @auftraege = auftraege
+  end
+
+  def stechen(stich)
+    stich.sieger.auftraege.each do |auftrag|
+      stich.karten.each do |karte|
+        auftrag.erfuellen(karte)
+      end
+    end
+    @auftraege.delete_if(&:erfuellt)
+    return unless @auftraege.length.zero?
+
+    @gewonnen = true
+  end
+end
