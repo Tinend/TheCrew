f5e6f9fd2d694715c3346b95aac011ab014c9266
Tinend <ulrich.brodowsky@gmx.net>
2022-12-28 19:31:40 +0100

    Richter geschrieben

--- lib/Hand.rb
+++ lib/Hand.rb
@@ -1,18 +1,18 @@
 class Hand
 
-  def initialize(karten: = [])
+  def initialize(karten: [])
     @karten = karten
   end
 
-  def ziehen(karte)
+  def ziehen(karte:)
     @karten.push(karte)
   end
 
-  def legen(karte)
+  def legen(karte:)
     @karten.delete(karte)
   end
   
-  def erlaubt?(karte, stich)
+  def erlaubt?(karte:, stich:)
     return false unless @karten.any? {|k| k == karte}
     return false if karte.farbe != stich.farbe and @karten.any?{|k| k.farbe == stich.farbe}
   end
--- lib/Richter.rb
+++ lib/Richter.rb
@@ -5,9 +5,23 @@ class Richter
     @auftraege = []
   end
 
+  attr_reader :gewonnen, :verloren
+  
   def auftragErhalten(auftrag)
     @auftraege.push(auftrag)
   end
 
-  def 
+  def stechen(stich)
+    stich.sieger.auftraege.each do |auftrag|
+      stich.karten.each do |karte|
+        auftrag.erfuellen(karte)
+      end
+    end
+    @auftraege.delete_if {|auftrag| auftrag.erfuellt}
+    if @auftraege.length == 0
+      @gewonnen = true
+    end
+  end
+
+  
 end
