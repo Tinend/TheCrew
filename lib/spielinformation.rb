ecfafe9ad31cc03dac41b4293e2230b0ed8a98b5
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2022-12-28 22:11:39 +0100

    Added Spielinformationssicht

--- lib/spielinformation.rb
+++ lib/spielinformation.rb
@@ -6,7 +6,7 @@
 # * Welche Spieler hat welche Aufträge.
 # * Welche Karten sind gegangen.
 class SpielInformation
-  def initialize(anzahl_spieler)
+  def initialize(anzahl_spieler:)
     @anzahl_spieler = anzahl_spieler
     @stiche = []
     @auftraege = Array.new(anzahl_spieler) { [] }
@@ -24,5 +24,32 @@ class SpielInformation
 
   def stich_fertig(stich)
     @stiche.push(stich)
+  end
+
+  def fuer_spieler(spieler_index)
+    SpielInformationsSicht.new(self, spieler_index)
+  end
+
+  class SpielInformationsSicht
+    def initialize(spiel_information:, spieler_index:)
+      @spiel_information = spiel_information
+      @spieler_index = spieler_index
+    end
+
+    def anzahl_spieler
+      @spiel_information.anzahl_spieler
+    end
+
+    def kapitaen
+      (@spiel_information.kapitaen - @spieler_index + @spiel_information.anzahl_spieler) % @spiel_information.anzahl_spieler
+    end
+
+    def auftraege
+      @spiel_information.auftraege.rotate(@spieler_index)
+    end
+
+    def stiche
+      @spiel_information.stiche
+    end
   end
 end
