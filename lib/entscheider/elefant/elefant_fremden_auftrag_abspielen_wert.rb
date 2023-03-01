9d7f5d20ffbd9eac6c5504d53a50b21e3b968afd
Tinend <ulrich.brodowsky@gmx.net>
2023-03-01 16:54:32 +0100

    zaehler_manager im Abspielen vom Elefant eingebaut

--- lib/entscheider/elefant/elefant_fremden_auftrag_abspielen_wert.rb
+++ lib/entscheider/elefant/elefant_fremden_auftrag_abspielen_wert.rb
@@ -4,30 +4,35 @@
 # berechnet Wert für Karten anspielen, wenn
 # Karte ein fremder Auftrag ist
 module ElefantFremdenAuftragAbspielenWert
-  def fremden_auftrag_abspielen_wert(karte:, stich:, ziel_spieler_index:)
+  def fremden_auftrag_abspielen_wert(karte:, stich:, ziel_spieler_index:, elefant_rueckgabe:)
     if hat_gespielt?(spieler_index: ziel_spieler_index, stich: stich)
-      vielleicht_auftrag_reinwerfen_abspielen_wert(karte: karte, stich: stich, ziel_spieler_index: ziel_spieler_index)
+      vielleicht_auftrag_reinwerfen_abspielen_wert(karte: karte, stich: stich, ziel_spieler_index: ziel_spieler_index,
+                                                   elefant_rueckgabe: elefant_rueckgabe)
     else
-      [0, 1, 0, 0, 10]
+      elefant_rueckgabe.symbol = :fremden_auftrag_holbar_abspielen
+      elefant_rueckgabe.wert = [0, 1, 0, 0, 10]
     end
   end
 
-  def vielleicht_auftrag_reinwerfen_abspielen_wert(karte:, stich:, ziel_spieler_index:)
+  def vielleicht_auftrag_reinwerfen_abspielen_wert(karte:, stich:, ziel_spieler_index:, elefant_rueckgabe:)
     if stich.sieger_index == ziel_spieler_index
-      auftrag_reinwerfen_abspielen_wert(karte: karte, stich: stich) # , ziel_spieler_index: ziel_spieler_index)
+      auftrag_reinwerfen_abspielen_wert(karte: karte, stich: stich, elefant_rueckgabe: elefant_rueckgabe) # , ziel_spieler_index: ziel_spieler_index)
     else
-      [0, -1, 0, 0, 0]
+      elefant_rueckgabe.symbol = :fremden_auftrag_vermasseln_abspielen
+      elefant_rueckgabe.wert = [0, -1, 0, 0, 0]
     end
   end
 
   # ziel_spieler_index:)
-  def auftrag_reinwerfen_abspielen_wert(karte:, stich:)
+  def auftrag_reinwerfen_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     if !karte.schlaegt?(stich.staerkste_karte) &&
        jeder_kann_unterbieten?(karte: stich.staerkste_karte,
                                end_index: @spiel_informations_sicht.anzahl_spieler - 1 - stich.length)
-      [0, 1, 0, 0, 10]
+      elefant_rueckgabe.symbol = :fremden_auftrag_sicher_abspielen
+      elefant_rueckgabe.wert = [0, 1, 0, 0, 10]
     else
-      [0, -1, 0, 0, 0]
+      elefant_rueckgabe.symbol = :fremden_auftrag_unsicher_abspielen
+      elefant_rueckgabe.wert = [0, -1, 0, 0, 0]
     end
   end
 end
--- lib/entscheider/elefant/elefant_keinen_auftrag_abspielen_wert.rb
+++ lib/entscheider/elefant/elefant_keinen_auftrag_abspielen_wert.rb
@@ -8,102 +8,118 @@ require_relative 'elefant_multiple_auftrag_farbe_abspielen_wert'
 module ElefantKeinenAuftragAbspielenWert
   include ElefantMultipleAuftragFarbeAbspielenWert
 
-  def keinen_auftrag_abspielen_wert(karte:, stich:)
+  def keinen_auftrag_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     auftraege_mit_farbe = auftraege_mit_farbe_berechnen(stich.farbe)
     eigene_auftraege_mit_farbe = auftraege_mit_farbe[0]
     fremde_auftraege_mit_farbe = auftraege_mit_farbe.sum - eigene_auftraege_mit_farbe
     if eigene_auftraege_mit_farbe.positive? && fremde_auftraege_mit_farbe.positive?
-      multiple_auftrag_farbe_abspielen_wert(karte: karte, stich: stich)
+      multiple_auftrag_farbe_abspielen_wert(karte: karte, stich: stich, elefant_rueckgabe: elefant_rueckgabe)
     elsif eigene_auftraege_mit_farbe.positive?
-      eigene_auftrag_stich_farbe_abspielen_wert(karte: karte)
+      eigene_auftrag_stich_farbe_abspielen_wert(karte: karte, elefant_rueckgabe: elefant_rueckgabe)
     elsif fremde_auftraege_mit_farbe.positive?
-      fremden_auftrag_stich_farbe_abspielen_wert(karte: karte, stich: stich)
+      fremden_auftrag_stich_farbe_abspielen_wert(karte: karte, stich: stich, elefant_rueckgabe: elefant_rueckgabe)
     else
-      keine_auftrag_stich_farbe_abspielen_wert(karte: karte, stich: stich)
+      keine_auftrag_stich_farbe_abspielen_wert(karte: karte, stich: stich, elefant_rueckgabe: elefant_rueckgabe)
     end
   end
 
-  def eigene_auftrag_stich_farbe_abspielen_wert(karte:)
-    [0, 1, 0, karte.schlag_wert, 0]
+  def eigene_auftrag_stich_farbe_abspielen_wert(karte:, elefant_rueckgabe:)
+    elefant_rueckgabe.symbol = :eigene_auftrag_stich_farbe_abspielen
+    elefant_rueckgabe.wert = [0, 1, 0, karte.schlag_wert, 0]
   end
 
-  def fremden_auftrag_stich_farbe_abspielen_wert(karte:, stich:)
+  def fremden_auftrag_stich_farbe_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     if (stich.farbe == karte.farbe) && karte.schlaegt?(stich.staerkste_karte)
-      [0, 0, -1, karte.schlag_wert, 0]
+      elefant_rueckgabe.symbol = :fremde_auftrag_stich_farbe_abspielen_schlagen
+      elefant_rueckgabe.wert = [0, 0, -1, karte.schlag_wert, 0]
     elsif karte.schlaegt?(stich.staerkste_karte)
-      [0, 0, -2, karte.schlag_wert, 0]
+      elefant_rueckgabe.symbol = :fremde_auftrag_stich_farbe_abspielen_trumpf
+      elefant_rueckgabe.wert = [0, 0, -2, karte.schlag_wert, 0]
     else
-      [0, 0, 1, karte.schlag_wert, 0]
+      elefant_rueckgabe.symbol = :fremde_auftrag_stich_farbe_abspielen_unterbieten
+      elefant_rueckgabe.wert = [0, 0, 1, karte.schlag_wert, 0]
     end
   end
 
-  def keine_auftrag_stich_farbe_abspielen_wert(karte:, stich:)
+  def keine_auftrag_stich_farbe_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     if karte.farbe == stich.farbe
-      keine_auftrag_stich_farbe_gleiche_farbe_abspielen_wert(karte: karte, stich: stich)
+      keine_auftrag_stich_farbe_gleiche_farbe_abspielen_wert(karte: karte, stich: stich, elefant_rueckgabe: elefant_rueckgabe)
     else
-      keine_auftrag_stich_farbe_andere_farbe_abspielen_wert(karte: karte)
+      keine_auftrag_stich_farbe_andere_farbe_abspielen_wert(karte: karte, elefant_rueckgabe: elefant_rueckgabe)
     end
   end
 
-  def keine_auftrag_stich_farbe_gleiche_farbe_abspielen_wert(karte:, stich:)
+  def keine_auftrag_stich_farbe_gleiche_farbe_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     if habe_noch_auftraege?
-      eigene_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte: karte, stich: stich)
+      eigene_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte: karte, stich: stich,
+                                                                                          elefant_rueckgabe: elefant_rueckgabe)
     else
-      fremde_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte: karte, stich: stich)
+      fremde_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte: karte, stich: stich,
+                                                                                          elefant_rueckgabe: elefant_rueckgabe)
     end
   end
 
-  def eigene_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte:, stich:)
+  def eigene_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     if karte.schlaegt?(stich.staerkste_karte)
-      [0, 0, 1, -karte.wert, 0]
+      elefant_rueckgabe.symbol = :eigene_auftraege_mit_fremder_farbe_unterstuetzen_schlagen_abspielen
+      elefant_rueckgabe.wert = [0, 0, 1, -karte.wert, 0]
     else
-      [0, 0, -1, -karte.wert, 0]
+      elefant_rueckgabe.symbol = :eigene_auftraege_mit_fremder_farbe_unterstuetzen_nicht_schlagen_abspielen
+      elefant_rueckgabe.wert = [0, 0, -1, -karte.wert, 0]
     end
   end
 
-  def fremde_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte:, stich:)
+  def fremde_auftraege_mit_anderer_stich_farbe_gleiche_farbe_unterstuetzen_abspielen_wert(karte:, stich:, elefant_rueckgabe:)
     if karte.schlaegt?(stich.staerkste_karte)
-      [0, 0, -1, karte.schlag_wert, 0]
+      elefant_rueckgabe.symbol = :fremde_auftraege_mit_fremder_farbe_unterstuetzen_schlagen_abspielen
+      elefant_rueckgabe.wert = [0, 0, -1, karte.schlag_wert, 0]
     else
-      [0, 0, 1, karte.schlag_wert, 0]
+      elefant_rueckgabe.symbol = :fremde_auftraege_mit_fremder_farbe_unterstuetzen_nicht_schlagen_abspielen
+      elefant_rueckgabe.wert = [0, 0, 1, karte.schlag_wert, 0]
     end
   end
 
-  def keine_auftrag_stich_farbe_andere_farbe_abspielen_wert(karte:)
+  def keine_auftrag_stich_farbe_andere_farbe_abspielen_wert(karte:, elefant_rueckgabe:)
     if karte.trumpf?
-      [0, 0, -2, 0, 0]
+      elefant_rueckgabe.symbol = :trumpf_auftragloser_stich_abspielen
+      elefant_rueckgabe.wert = [0, 0, -2, 0, 0]
     elsif habe_noch_auftraege?
-      eigene_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte: karte)
+      eigene_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte: karte, elefant_rueckgabe: elefant_rueckgabe)
     else
-      fremde_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte: karte)
+      fremde_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte: karte, elefant_rueckgabe: elefant_rueckgabe)
     end
   end
 
-  def eigene_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte:)
+  def eigene_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte:, elefant_rueckgabe:)
     if @spiel_informations_sicht.unerfuellte_auftraege_mit_farbe(karte.farbe)[0].empty?
-      [0, 0, 0, -karte.wert, 0]
+      elefant_rueckgabe.symbol = :eigene_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen
+      elefant_rueckgabe.wert = [0, 0, 0, -karte.wert, 0]
     else
-      verlorene_farbe_mit_auftrag_abspielen_wert(karte: karte)
+      verlorene_farbe_mit_auftrag_abspielen_wert(karte: karte, elefant_rueckgabe: elefant_rueckgabe)
     end
   end
 
-  def verlorene_farbe_mit_auftrag_abspielen_wert(karte:)
+  def verlorene_farbe_mit_auftrag_abspielen_wert(karte:, elefant_rueckgabe:)
     min_auftrag = @spiel_informations_sicht.unerfuellte_auftraege_mit_farbe(karte.farbe)[0].min_by do |auftrag|
       auftrag.karte.wert
     end
     max_karte = @spiel_informations_sicht.karten_mit_farbe(karte.farbe).max
     if max_karte.wert >= min_auftrag.karte.wert
-      [0, 0, -1, -karte.wert, 0]
+      elefant_rueckgabe.symbol = :verlorene_farbe_mit_holbarer_auftrag_farbe_abspielen
+      elefant_rueckgabe.wert = [0, 0, -1, -karte.wert, 0]
     else
-      [0, 0, 1, 0, 0]
+      elefant_rueckgabe.symbol = :verlorene_farbe_mit_unholbarer_auftrag_farbe_abspielen
+      elefant_rueckgabe.wert = [0, 0, 1, 0, 0]
     end
   end
 
-  def fremde_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte:)
+  def fremde_auftraege_mit_verlorener_farbe_unterstuetzen_abspielen_wert(karte:, elefant_rueckgabe:)
     if @spiel_informations_sicht.unerfuellte_auftraege_mit_farbe(karte.farbe).flatten.empty?
-      [0, 0, 0, karte.wert, 0]
+      elefant_rueckgabe.symbol = :verlorene_farbe_mit_fremder_holbarer_auftrag_farbe_abspielen
+      elefant_rueckgabe.wert = [0, 0, 0, karte.wert, 0]
     else
-      [0, 0, 1, karte.wert, 0]
+      elefant_rueckgabe.symbol = :verlorene_farbe_mit_fremder_unholbarer_auftrag_farbe_abspielen
+      elefant_rueckgabe.wert = [0, 0, 1, karte.wert, 0]
     end
   end
 end
